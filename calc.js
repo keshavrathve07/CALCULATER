6b94a5c1c9f63c86b7234e0b8a67d38a8d83457f
keshavrathve07 <rathvekeshav@gmail.com>
2024-11-29 14:11:06 +0530

    CALC V1

---
+++ calc.js
@@ -0,0 +1,40 @@
+var myInputBox = document.getElementById("calcinputbox");
+function input1() {
+    myInputBox.value += "1"
+}
+function input2() {
+    myInputBox.value += "2"
+}
+function input3() {
+    myInputBox.value += "3"
+}
+function input4() {
+    myInputBox.value += "4"
+}
+function input5() {
+    myInputBox.value += "5"
+}
+function input6() {
+    myInputBox.value += "6"
+}
+function input7() {
+    myInputBox.value += "7"
+}
+function input8() {
+    myInputBox.value += "8"
+}
+function input9() {
+    myInputBox.value += "9"
+}
+function inputPlus() {
+    myInputBox.value += "+"
+}
+function inputMinus() {
+    myInputBox.value += "-"
+}
+
+function calculate() {
+    myInputBox.value = eval(myInputBox.value)
+}
+
+
